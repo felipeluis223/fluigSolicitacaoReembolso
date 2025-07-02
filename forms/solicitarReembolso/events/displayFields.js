73f4eb24e96fa3016aa5989ab765cb243ecad26b
felipeluis223 <lf9072962@gmail.com>
2025-07-02 09:44:58 -0300

    chore: removendo comentarios.

--- forms/solicitarReembolso/events/displayFields.js
+++ forms/solicitarReembolso/events/displayFields.js
@@ -6,49 +6,33 @@ function displayFields(form, customHTML) {
     var groups = ["grpColaboradores", "grpCentroCusto", "grpFinanceiro"];
     var userGroup = {};
 
-    log.info("CHECKDATA 1");
+    // Percorrer os grupos que o usuário participa:
     for (var i = 0; i < groupDataset.rowsCount; i++) {
-        log.info("CHECKDATA 1.1");
         var groupId = groupDataset.getValue(i, "colleagueGroupPK.groupId");
         for (var index = 0; index < groups.length; index++) {
-            log.info("CHECKDATA 1.2");
             if (groupId == groups[index]) {
-                log.info("CHECKDATA 1.2.1");
                 userGroup[groups[index]] = groupId;
             }
         }
     }
 
-    log.info("CHECKDATA 2");
-    function getDate() {
-        var now = new Date();
-        var day = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-        var month = now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
-        return day + '/' + month + '/' + now.getFullYear();
-    }
-
     // Etapa: Colaborador
-    if ("grpColaboradores" in userGroup) {
-        log.info("CHECKDATA 3 - Colaborador");
+    if ("grpColaboradores" in userGroup) {        
+        // ID e Nome com os dados obtidos do sistema:
+        form.setValue("idSolicitante", user);        
+        form.setEnabled("idSolicitante", false);
+        form.setValue("nomeSolicitante", user);
+        form.setEnabled("nomeSolicitante", false);
         
-        // form.setValue("idSolicitante", user);        
-        // form.setEnabled("idSolicitante", true);
-        
-        
-        // form.setValue("nomeSolicitante", user);
-        // form.setEnabled("nomeSolicitante", true);
-
         form.setEnabled("valor", true);
         form.setEnabled("centroCusto", true);
         form.setEnabled("dataDespesa", true);
         form.setEnabled("justificativa", true);
         form.setEnabled("anexoDespesas", true);
-        log.info("CHECKDATA 3 - Colaborador - FIM");
     }
 
     // Etapa: Financeiro
     if ("grpFinanceiro" in userGroup) {
-        log.info("CHECKDATA 3 - Financeiro");
         form.setEnabled("idFinanceiro", true);
         form.setEnabled("nomeFinanceiro", true);
         form.setEnabled("dataFinanceiro", true);
